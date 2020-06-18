c1931f8d3bc362011d6ad8f93fd1b496eb97f1b8
Jack Kennedy <jack.kennedy@gmail.com>
2020-06-18 05:28:27 -0400

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable, Logger } from '@nestjs/common';
 export class AppService {
 
   getSections(timezone) {
-    Logger.log('New Get Sections');
+    Logger.log('AAAA Get Sections');
     const expires = Date.now() + 10000;
 
     var sections =
