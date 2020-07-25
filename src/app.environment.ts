9e33c4d86e798d59a628b5c71ce7845a27885f09
Jack Kennedy <jack.kennedy@gmail.com>
2020-07-25 10:42:23 -0400

    fix

--- src/app.environment.ts
+++ src/app.environment.ts
@@ -1,5 +1,6 @@
 const local = false;
 
 export function baseUrl() {
+  console.log('baseUrl: local:' + local);
   return local ? 'http://localhost:3000' : 'https://api.jackkennedy.info';
 }
--- src/google.controller.ts
+++ src/google.controller.ts
@@ -17,6 +17,8 @@ export class GoogleController {
     let uuid = params.uuid;
     let encodedBaseUrl = encodeURIComponent(env.baseUrl());
 
+    console.log(encodedBaseUrl);
+
     let authUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.readonly&access_type=offline&include_granted_scopes=true&state=${uuid}&redirect_uri=${encodedBaseUrl}%2Fgoogle%2Fredirect&response_type=code&client_id=359440454777-4hecg7ig1iloj5u1q2iaanuqb9gj6f7d.apps.googleusercontent.com`;
 
     res.redirect(authUrl);
